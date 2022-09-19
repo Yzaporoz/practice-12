13fdd9c01c2642cbda081ff4de9da39b2138a9c5
unknown <j0982649910@gmail.com>
2022-09-19 14:30:22 +0300

    Добавляет файлы Live-server

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const math = require("./math");
+
+console.log(math.add(4, 5));
+console.log(math.divine(100, 5));
---
+++ math.js
@@ -0,0 +1,7 @@
+
+exports.add =  (x, y) => (x + y);
+
+exports.divine =function divine (x, y) {
+    return x / y;
+}
+
